a7652c869f17cf9b48ddb40ac1d36ae7072f031e
unknown <uriel2014>
2023-04-05 18:04:20 -0500

    se sube el primer cambio y solo faltante a que salga las vistas en el servidor

---
+++ src/app.py
@@ -0,0 +1,28 @@
+#https://docs.sqlalchemy.org/en/14/core/type_basics.html
+#https://flask.palletsprojects.com/en/2.2.x/
+#from api.user import *
+from flask import Flask,  redirect, request, jsonify, json, session, render_template
+from db.db import db, app, ma
+
+
+
+from tablas.primera import primeras, primeraSchema
+
+
+from rutas.primer import routes_primer
+
+app.register_blueprint(routes_primer, url_prefix="/fronted")
+
+#esto para que corra el server y ayuda con el puerto
+if __name__ == '__main__':
+   # load_dotenv()
+    app.run(debug=True, port=5000, host='0.0.0.0')
+    
+
+primer_schema = primeraSchema()
+primeras_schema = primeraSchema(many=True)
+
+@app.route('/index', methods=['GET'] )
+def indexprimer():
+    
+    return "Hola Mundo!!"
---
+++ src/rutas/primer.py
@@ -0,0 +1,25 @@
+from db.db import db, app, ma
+from flask import Blueprint, Flask,  redirect, request, jsonify, json, session, render_template
+
+routes_primer = Blueprint("routes_primera", __name__)
+from tablas.primera import primeras, primeraSchema
+
+@routes_primer.route('/indexprimer', methods=['GET'] )
+def indexinstitution():
+    
+    return render_template('/vistas/index.html')
+
+
+@routes_primer.route('/guardarinstitution',methods=['POST'])
+def saveinstitution():
+    #request.form['title']
+
+    #en el fullname va el dato de la db y en Roles de usuarios va la tbala donde se sacan los datos de la db
+    nombre = request.form['nombre']
+    apelldio = request.form['apelldio']
+    print(nombre,nombre)
+    new_primer = primeras(nombre)
+    db.session.add(new_primer)
+    db.session.commit()
+    return nombre
+    
